b51563eaa61f9a1177e7fa67b2e1fd96e9cea81b
YuukaDev <vule.amv1@gmail.com>
2021-05-27 17:28:54 +0200

    Updated JS

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert('da')
+let btn = document.getElementById('button');
+let text = document.getElementById('container-text');
+
+function changeColor() {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = `#${randomColor}`;
+    text.innerHTML = `Background Color : #${randomColor}`;
+}
+
+btn.addEventListener('click', changeColor);
